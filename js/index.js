a3a1a0eba9231fe46dda84b4a9cece257e2e7d47
Ingramb4 <breana.marie.ingram@gmail.com>
2023-05-11 21:38:36 -0400

    commit to main

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 console.log("Your index.js file is loaded correctly!");
 
 
-$(document).ready(function(){
-    $("#p1").hover(function(){
-      alert("Welcome to my Portfolio!");
-    },
-    function(){
-      alert("Thank you so much for visiting!");
-    }); 
-  });
+// $(document).ready(function(){
+//     $("#p1").hover(function(){
+//       alert("Welcome to my Portfolio!");
+//     },
+//     function(){
+//       alert("Thank you so much for visiting!");
+//     }); 
+//   });
 
+document.getElementById("p1").addEventListener("click", function() {
+  alert("Welcome to my Portfolio!");
+});
 
